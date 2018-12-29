02e9c19f6681b1f66a580327db63116c06726d21
Weicheng Yang <445919439@qq.com>
2018-12-29 11:31:04 +0800

    基本完成

--- LittleLights/ViewController.m
+++ LittleLights/ViewController.m
@@ -12,21 +12,47 @@
 @property (weak, nonatomic) IBOutlet UITextField *securityCodeTextField;
 @property (weak, nonatomic) IBOutlet UIButton *getSecurityCodeButton;
 @property (weak, nonatomic) IBOutlet UIButton *loginButton;
-
+@property (assign, nonatomic) NSInteger countDown;
 @end
 
 @implementation ViewController
 
+- (IBAction)getSecurityButtonClicked:(id)sender {
+    self.countDown = 10;
+    [self.getSecurityCodeButton setTitle:[NSString stringWithFormat:@"%ld",self.countDown] forState:UIControlStateDisabled];
+    @weakify(self);
+    [NSTimer scheduledTimerWithTimeInterval:1 repeats:true block:^(NSTimer * _Nonnull timer) {
+        @strongify(self);
+        self.countDown--;
+        [self.getSecurityCodeButton setTitle:[NSString stringWithFormat:@"%ld",self.countDown] forState:UIControlStateDisabled];
+        if (self.countDown == 0) {
+            [timer invalidate];
+            [self.getSecurityCodeButton setTitle:@"获取验证码" forState:UIControlStateDisabled];
+        }
+    }];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // 仅当手机号11位数字、验证码6位数字时，登录按钮高亮
-    RAC(self.loginButton, enabled) = [RACSignal combineLatest:@[self.phoneTextField.rac_textSignal, self.securityCodeTextField.rac_textSignal] reduce:^id (NSString *phone, NSString *code){
-        NSLog(@"%@ %@",phone, code);
-        return @(phone.length == 11 && code.length == 6);
-    }];
-    // 仅当手机号11位数字时，获取验证码按钮高亮
-    RAC(self.getSecurityCodeButton, enabled) = [self.phoneTextField.rac_textSignal map:^id _Nullable(NSString * _Nullable value) {
+    RACSignal *validPhoneLength = [self.phoneTextField.rac_textSignal map:^id _Nullable(NSString * _Nullable value) {
         return @(value.length == 11);
     }];
+    RACSignal *validCodeLength = [self.securityCodeTextField.rac_textSignal map:^id _Nullable(NSString * _Nullable value) {
+        return @(value.length == 6);
+    }];
+    RACSignal *isCountingDown = [RACObserve(self, countDown) map:^id _Nullable(NSNumber *_Nullable value) {
+        return @(![value isEqualToNumber:@0]);
+    }];
+    
+    // 仅当手机号11位数字时，获取验证码按钮高亮
+    RAC(self.getSecurityCodeButton, enabled) = [RACSignal combineLatest:@[validPhoneLength, isCountingDown] reduce:^id (NSNumber *validPhoneLength, NSNumber *isCountingDown){
+        return @(validPhoneLength.boolValue && (!isCountingDown.boolValue));
+    }];
+    
+    // 仅当手机号11位数字、验证码6位数字时，登录按钮高亮
+    RAC(self.loginButton, enabled) = [RACSignal combineLatest:@[validPhoneLength, validCodeLength] reduce:^id (NSNumber *validPhoneLength, NSNumber *validCodeLength){
+        return @(validPhoneLength.boolValue && validCodeLength.boolValue);
+    }];
+    
 }
 @end
